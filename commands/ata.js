5c97e1c79843126780d0fe0b3bd0eeca38a7f193
fvsgamers <fvsgamers@gmail.com>
2026-03-28 13:47:10 -0300

    Update ata.js

--- commands/ata.js
+++ commands/ata.js
@@ -1,5 +1,6 @@
 const { Command } = require('@sapphire/framework');
 const { EmbedBuilder } = require('discord.js');
+const ID_DO_CANAL = 1485775547723284571;
 
 module.exports = class AtaCommand extends Command {
   constructor(context, options) {
@@ -64,7 +65,7 @@ module.exports = class AtaCommand extends Command {
       )
       .setTimestamp();
 
-    const canal = interaction.guild.channels.cache.get('1485775547723284571');
+    const canal = interaction.guild.channels.cache.get('ID_DO_CANAL');
 
     await canal.send({ embeds: [embed] });
 
